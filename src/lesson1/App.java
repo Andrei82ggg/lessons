7e972142eb628a7254d7a127c9e6d81c53d77de6
Professional <ustu1982@mail.ru>
2023-05-28 21:42:08 +0500

    fixedd

--- src/lesson1/App.java
+++ src/lesson1/App.java
@@ -6,7 +6,6 @@ public class App {
         Aftor michail = new Aftor("Михаил", "Булгаков");
         Aftor lev =new Aftor("Лев", "Толстой");
         Book master = new Book("Мастер", michail, 1930);
-
         master.setYaer(200);
 
         System.out.println(master.getTitle() + " "+ master.getAftor().getFirstName() + " "+michail.getLastName() + " "+master.getYaer());
