1d42279115524bc8ae2e8c55c064a433a1f945e1
Professional <ustu1982@mail.ru>
2023-05-29 10:07:27 +0500

    fix

--- src/lesson1/App.java
+++ src/lesson1/App.java
@@ -3,11 +3,9 @@ package lesson1;
 public class App {
     public static void main(String[] args){
 
-        Aftor michail = new Aftor("Михаил", "Булгаков");
         Aftor lev =new Aftor("Лев", "Толстой");
-        Book master = new Book("Мастер", michail, 1930);
-        master.setYaer(200);
-
-        System.out.println(master.getTitle() + " "+ master.getAftor().getFirstName() + " "+michail.getLastName() + " "+master.getYaer());
+        Book warAndPeace = new Book("Война и мир", lev, 1930);
+        Book master = new Book("Мастер", new Aftor("Михаил", "Булгаков"), 1930);
+        System.out.println(master.getTitle() + " "+ master.getAftor().getFirstName() +" " +master.getAftor().getLastName()+" "+master.getYaer());
     }
 }
