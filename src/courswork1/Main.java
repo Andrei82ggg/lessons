edb45f1936e106e1ef9801690fe97ca7ea57b695
Professional <ustu1982@mail.ru>
2023-06-07 20:40:31 +0500

    coursase

--- src/courswork1/Main.java
+++ src/courswork1/Main.java
@@ -2,8 +2,9 @@ package courswork1;
 
 public class Main {
     private static final Employee[] employees = new Employee[10];
-    public static void main(String[] args){
-        employees[0] = new Employee("Иванов Иван Иванович", 1, 55000);
+
+    public static void main(String[] args) {
+        employees[0] = new Employee("Котов Петр Петрович", 1, 613300);
         employees[1] = new Employee("Петров Петр Петрович", 1, 61000);
         employees[2] = new Employee("Симонов Иван Иванович", 2, 62000);
         employees[3] = new Employee("Когель Юлия Ивановна", 2, 63000);
@@ -15,31 +16,64 @@ public class Main {
         employees[9] = new Employee("Лиман Екатерина Андреевна", 5, 60412);
         printAll();
         System.out.println("Сумма затрат в месяц = " + sum());
+        System.out.println("Средняя сумма = " + average());
         findMinSalary();
+
+        Employee minSalary = findMinSalary();
+        if (minSalary != null) {
+            System.out.println("Сотрудник с минимальной зарплатой " + minSalary);
+        }
+        findMaxSalary();
+        printFIO();
     }
-    static void printAll(){
-        for (Employee employee : employees){
+
+    static void printAll() {
+        for (Employee employee : employees) {
             System.out.println(employee);
         }
     }
-    static int sum(){
+
+    static int sum() {
         int sum = 0;
-        for (Employee employee : employees){
+        for (Employee employee : employees) {
             sum += employee.getSalari();
         }
         return sum;
     }
-    static void  findMinSalary(){
-        int min = Integer.MAX_VALUE;
-        int index = 0;
-        for (int i = 0; i < employees.length; i++) {
-            if (employees[i].getSalari() < min) {
-                min = employees[i].getSalari();
-                index = i;
+
+    static Employee findMinSalary() {
+        Employee min = null;
+        for (Employee employee : employees) {
+            if (employee == null) {
+                continue;
+            }
+            if (min == null || employee.getSalari() < min.getSalari()) {
+                min = employee;
             }
         }
-        System.out.println("Минимальная зарплата =" + min);
-        System.out.println("Сотрудник с минимальной зарплатой" + min);
+        return min;
 
     }
+
+    static void findMaxSalary() {
+        Employee max = null;
+        for (Employee employee : employees) {
+            if (employee == null) {
+                continue;
+            }
+            if (max == null || employee.getSalari() > max.getSalari()) {
+                max = employee;
+            }
+        }
+        System.out.println("Сотрудник с максимальной зарплатой " +max);
+    }
+    static double average(){
+        return (double) sum() / employees.length;
+    }
+    static void printFIO(){
+        for (Employee employee : employees){
+            System.out.println(employee.getFio());
+        }
+    }
 }
+
